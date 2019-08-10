731cf099b428bf37397059bdff6803fc340327be
Aibek Ozhorov <ozhorov@gmail.com>
2019-08-10 21:54:49 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,21 +1,25 @@
 def addition(num1, num2)
-
+5+4
+result = 9
 end
 
 def subtraction(num1, num2)
-
+10-5
+result = 5
 end
 
 def division(num1, num2)
-
+50/2
+result = 25
 end
 
 def multiplication(num1, num2)
-
+4*30
+result = 120
 end
 
 def modulo(num1, num2)
-
+34
 end
 
 def square_root(num)
